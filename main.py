4e0c552f8b8a577852f82b39b6e317e1087e5b91
raja7771912 <raja7771912@outlook.com>
2026-08-10 12:16:43 +0530

    Change Telegram bot token
    Updated the bot token for the Telegram bot.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, ChatJoinRequestHandler, ContextTypes
 
-BOT_TOKEN = "8811657177:AAETGlMNLbId4pnXHe37P4pY3TsWiTMmzek"
+BOT_TOKEN = "8640329086:AAHq0Zf2wVGID6ARP8TJkSJP4kE9j8sx93w"
 
 async def approve_request(update: Update, context: ContextTypes.DEFAULT_TYPE):
     req = update.chat_join_request
